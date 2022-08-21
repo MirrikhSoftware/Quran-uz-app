1bd7cf17c5e07961cc3461e88c7009aa32d11d8b
BahromjonPolat <itjunior.uz@gmail.com>
2022-08-21 15:26:23 +0500

    constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/rendering.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFF1E7D0);
+  static const Color primary = Color(0xFF315881);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+}
