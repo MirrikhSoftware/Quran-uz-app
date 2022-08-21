43ba028760fd77428819ada3ec793485d15ed1f9
BahromjonPolat <itjunior.uz@gmail.com>
2022-08-21 19:35:42 +0500

    SuraScreen was modified

--- lib/core/components/app_packages.dart
+++ lib/core/components/app_packages.dart
@@ -3,3 +3,4 @@ export 'package:bloc/bloc.dart';
 export 'package:hive_flutter/hive_flutter.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:flutter_svg/flutter_svg.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
--- lib/view/screens/sura/sura_screen.dart
+++ lib/view/screens/sura/sura_screen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:quran/bloc/sura/sura_bloc.dart';
+import 'package:quran/core/components/app_packages.dart';
 
 class SuraScreen extends StatelessWidget {
-  const SuraScreen({ Key? key }) : super(key: key);
+  const SuraScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(),
+    return BlocBuilder<SuraBloc, SuraState>(
+      builder: (context, state) {
+        SuraBloc suraBloc = context.watch();
+        Sura sura = suraBloc.sura;
+        return Scaffold(
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                title: Text(sura.nameUz!),
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate([]),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
