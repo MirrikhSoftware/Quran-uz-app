f84171fa2697cdc0c7d642c865b057d74a360997
BahromjonPolat <itjunior.uz@gmail.com>
2022-08-21 15:50:53 +0500

    AppIcons and images

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,14 @@
+class AppIcons {
+  static const String _path = 'assets/icons';
+  static const String book = '$_path/book.svg';
+  static const String bookmark = '$_path/bookmark.svg';
+  static const String dua = '$_path/dua.svg';
+  static const String lamp = '$_path/lamp.svg';
+  static const String hexagon = '$_path/hexagon.svg';
+  static const String play = '$_path/play.svg';
+  static const String prayer = '$_path/prayer.svg';
+  static const String search = '$_path/search.svg';
+  static const String share = '$_path/share.svg';
+  static const String sort = '$_path/sort.svg';
+
+}
---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const String _path = 'assets/images';
+  static const String book = '$_path/book.svg';
+  static const String sky = '$_path/sky.svg';
+  static const String basmalah = '$_path/Basmalah.svg';
+}
